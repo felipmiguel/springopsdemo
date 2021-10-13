788ea04f7995ef109aa719f8932037108bfa5a57
felipmiguel <a@b.c>
2021-10-13 16:44:06 +0200

    first commit

---
+++ src/main/java/com/demo/springopsdemo/SpringopsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringopsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringopsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springopsdemo/SpringopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
