afdcce6e43f1f92faf39f7fe4657d0f9206a7981
felipmiguel <fmiguel@microsoft.com>
2021-10-18 17:39:45 +0200

    sample

--- src/main/java/com/demo/springopsdemo/HelloController.java
+++ src/main/java/com/demo/springopsdemo/HelloController.java
@@ -1,6 +1,7 @@
 package com.demo.springopsdemo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,25 @@ public class HelloController {
     @Value("${application.message:Not configured by a Spring Cloud Server}")
     private String message;
 
+    @Value("${SPRING_DATASOURCE_USERNAME: SPRING_DATASOURCE_USERNAME didn't work}")
+    private String userName;
+
+    @Value("${database-username: database-username didn't work}")
+    private String userNameFromSecret;
+
     @GetMapping("/hello")
     public String hello() {
         return message + '\n';
     }
+
+    @GetMapping("/env")
+    public String getEnv(String v) {
+        return System.getenv(v);
+    }
+
+    @GetMapping("/vars")
+    public String getVars(){
+        return userName + " | " + userNameFromSecret;
+    }
+
 }
